07a377fa1888da4c26135cbb31e76be0c8d59291
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2023-03-25 07:04:34 -0300

    add config types default styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
-
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 function App() {
 
   return (
-    <></>
+    <ThemeProvider theme={defaultTheme}>
+
+    </ThemeProvider>
   )
 }
 
