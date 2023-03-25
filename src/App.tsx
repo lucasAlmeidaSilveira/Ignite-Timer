1acaa54a83b3677144e232c1e4d936ca9272e510
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2023-03-25 07:23:01 -0300

    add script eslint in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { ThemeProvider } from 'styled-components';
-import { GlobalStyle } from './styles/global';
-import { defaultTheme } from './styles/themes/default';
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
