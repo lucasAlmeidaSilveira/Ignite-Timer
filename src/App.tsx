ab307fbc4ddfedb56a494f4d157a6b9f646657ab
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2023-03-26 22:46:28 -0300

    add config react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
